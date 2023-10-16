6a79cd7b0f585254f8089f55bd7b18028b74e992
Mark Jaquith <mark@jaquith.me>
2023-10-16 14:48:24 -0400

    Update debug

--- src/lib/server/handleClerk.ts
+++ src/lib/server/handleClerk.ts
@@ -17,21 +17,23 @@ export default function handleClerk(
 		createClient(secretKey)
 		const sessionToken = event.cookies.get('__session')
 
+		debug && console.log('[Clerk SvelteKit] ' + event.url.pathname)
+
 		if (sessionToken) {
-			debug && console.log('[Server Hook] Found session token in cookies.')
+			debug && console.log('[Clerk SvelteKit] Found session token in cookies.')
 			try {
 				const session = await verifySession(sessionToken)
 				if (session) {
-					debug && console.log('[Server Hook] Session verified successfully.')
+					debug && console.log('[Clerk SvelteKit] Session verified successfully.')
 					event.locals.session = session
 				} else {
-					debug && console.warn('[Server Hook] Session verification returned no session.')
+					debug && console.warn('[Clerk SvelteKit] Session verification returned no session.')
 				}
 			} catch (reason) {
-				console.warn('[Server Hook] Warning during session verification:', reason)
+				console.warn('[Clerk SvelteKit] Warning during session verification:', reason)
 			}
 		} else {
-			debug && console.log('[Server Hook] No session token found in cookies.')
+			debug && console.log('[Clerk SvelteKit] No session token found in cookies.')
 		}
 
 		// Protect the protected routes.
@@ -39,7 +41,7 @@ export default function handleClerk(
 			!event.locals.session &&
 			protectedPaths.find((path) => event.url.pathname.startsWith(path))
 		) {
-			debug && console.log('[Server Hook] No session found, redirecting to login screen.')
+			debug && console.log('[Clerk SvelteKit] No session found, redirecting to login screen.')
 			throw redirect(303, signInUrl)
 		}
 
