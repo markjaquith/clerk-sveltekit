801e8ec4fe58014e8ef0baeb4c5cfd111ffdd848
Brian McIlwain <technicallyemployed@gmail.com>
2023-10-27 20:07:26 -0600

    Try to add more named exports
    Don't know if this will work

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,11 @@
+import ClerkLoaded from './client/ClerkLoaded.svelte'
+import ClerkLoading from './client/ClerkLoading.svelte'
+import SignedIn from './client/SignedIn.svelte'
+import SignedOut from './client/SignedOut.svelte'
+import SignIn from './client/SignIn.svelte'
+import SignUp from './client/SignUp.svelte'
+import UserButton from './client/UserButton.svelte'
+
 export { clerkUI, initializeClerkClient } from './client/index.js'
+
+export { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignIn, SignUp, UserButton }
