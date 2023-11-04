e7bf1f7d8e192c64d744e35e922ba79c7f962edf
Mark Jaquith <mark@jaquith.me>
2023-11-04 14:46:06 -0400

    Implement SignInButton, SignUpButton, and SignOutButton
    fixes #2
    fixes #3
    fixes #4

---
+++ src/lib/client/SignOutButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { clerk } from '../client/index.js'
+	import type { HTMLButtonAttributes } from 'svelte/elements'
+
+	export let signOutCallback: (() => void) | undefined = undefined
+	export let signOutOptions: Record<string, unknown> | undefined = undefined
+
+  interface $$Props extends HTMLButtonAttributes {
+		signOutCallback?: (() => void) | undefined
+		signOutOptions?: Record<string, unknown> | undefined
+  }
+	
+	function signOut() {
+		return $clerk?.signOut(signOutCallback, signOutOptions)
+	}
+</script>
+
+<button type="button" on:click={signOut} {...$$restProps}><slot>Sign Out</slot></button>
---
+++ src/lib/client/SignUpButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { clerk } from '../client/index.js'
+	import type { HTMLButtonAttributes } from 'svelte/elements'
+
+	export let afterSignInUrl: string | undefined = undefined
+	export let afterSignUpUrl: string | undefined = undefined
+	export let redirectUrl: string | undefined = undefined
+	export let mode: "redirect" | "modal" | undefined = undefined
+
+  interface $$Props extends HTMLButtonAttributes {
+		afterSignInUrl?: string | undefined
+		afterSignUpUrl?: string | undefined
+		redirectUrl?: string | undefined
+		mode?: "redirect" | "modal" | undefined
+  }
+	
+	function signUp() {
+		const opts = { afterSignInUrl, afterSignUpUrl, redirectUrl }
+		if (mode === 'modal') {
+			return $clerk?.openSignUp(opts)
+		}
+		return $clerk?.redirectToSignUp(opts)
+	}
+</script>
+
+<button type="button" on:click={signUp} {...$$restProps}><slot>Sign Up</slot></button>
