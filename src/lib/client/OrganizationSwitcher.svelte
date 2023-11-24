c5f666cb80a3418ffac293226c23cdf9c387c0cd
Mark Jaquith <mark@jaquith.me>
2023-11-24 15:52:48 -0500

    Add CreateOrganization, OrganizationProfile, and OrganizationSwitcher components
    fixes #31

---
+++ src/lib/client/OrganizationSwitcher.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import clerkUI from './clerkui.js'
+	import type { OrganizationSwitcherProps } from '@clerk/types'
+	import ClerkLoaded from './ClerkLoaded.svelte'
+
+	type $$Props = OrganizationSwitcherProps
+</script>
+
+<ClerkLoaded let:clerk>
+	<div use:clerkUI={{ clerk, componentType: 'OrganizationSwitcher', props: {...$$props }}} />
+</ClerkLoaded>
