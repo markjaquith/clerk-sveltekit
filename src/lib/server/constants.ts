b161496426912db2777944482e9c287f52bcf513
wobsoriano <sorianorobertc@gmail.com>
2024-08-06 09:35:02 -0700

    chore: Update env vars

--- src/lib/server/constants.ts
+++ src/lib/server/constants.ts
@@ -1,11 +1,11 @@
 import { constants } from '@clerk/backend/internal'
-import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public'
-import { env } from '$env/dynamic/private'
+import { env as privateEnv } from '$env/dynamic/private'
+import { env as publicEnv } from '$env/dynamic/public'
 
-export const API_VERSION = env.CLERK_API_VERSION || 'v1'
-export const SECRET_KEY = env.CLERK_SECRET_KEY || ''
-export const PUBLISHABLE_KEY = PUBLIC_CLERK_PUBLISHABLE_KEY || ''
-export const API_URL = env.CLERK_API_URL
-export const JWT_KEY = env.CLERK_JWT_KEY || ''
+export const API_VERSION = publicEnv.PUBLIC_CLERK_API_VERSION || 'v1'
+export const SECRET_KEY = privateEnv.CLERK_SECRET_KEY || ''
+export const PUBLISHABLE_KEY = publicEnv.PUBLIC_CLERK_PUBLISHABLE_KEY || ''
+export const API_URL = publicEnv.PUBLIC_CLERK_API_URL
+export const JWT_KEY = privateEnv.CLERK_JWT_KEY || ''
 
 export const { Cookies, Headers } = constants
