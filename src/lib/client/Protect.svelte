d3e4c3f18bac80ecf88a1b3d9f3e7865a673006f
wobsoriano <sorianorobertc@gmail.com>
2024-08-06 07:25:17 -0700

    chore: Simplify Protect component

--- src/lib/client/Protect.svelte
+++ src/lib/client/Protect.svelte
@@ -1,49 +1,47 @@
 <script lang="ts">
     import type { CheckAuthorizationWithCustomPermissions, OrganizationCustomPermissionKey, OrganizationCustomRoleKey } from '@clerk/types'
     import ClerkLoaded from './ClerkLoaded.svelte'
-    import { user, organization } from './store.js'
+    import { auth } from './store.js'
 
     export let condition: ((has: CheckAuthorizationWithCustomPermissions) => boolean) | undefined = undefined
     export let role: OrganizationCustomRoleKey | undefined = undefined
     export let permission: OrganizationCustomPermissionKey | undefined = undefined
 
-	$: membership = $organization ? $user?.organizationMemberships?.find(om => om.organization.id === $organization.id) : $organization
-
 	const has = (params: Parameters<CheckAuthorizationWithCustomPermissions>[0]) => {
       if (!params?.permission && !params?.role)
         throw new Error('Missing parameters. The prop permission or role is required to be passed. Example usage: `has({permission: "org:posts:edit"`')
-      if (!$organization?.id || !$user?.id || !membership?.role || !membership?.permissions)
+      if (!$auth.orgId || !$auth.userId || !$auth.orgRole || !$auth.orgPermissions)
         return false
 
       if (params.permission)
-        return membership.permissions.includes(params.permission)
+        return $auth.orgPermissions.includes(params.permission)
 
       if (params.role)
-        return membership.role === params.role
+        return $auth.orgRole === params.role
 
       return false
     }
 
 	$: isAuthorized = () => {
+	  if (!$auth.userId) return false;
+
       if (typeof condition === 'function') {
         return condition(has);
       }
+
       if (role || permission) {
-        return has?.({ role: membership.role, permission });
+        return has?.({ role, permission } as Parameters<CheckAuthorizationWithCustomPermissions>[0]);
       }
+
       return true
     };
 </script>
 
 
 <ClerkLoaded>
-  {#if $user?.id}
     {#if isAuthorized()}
-      <slot />
+        <slot />
     {:else}
-      <slot name="fallback" />
+        <slot name="fallback" />
     {/if}
-  {:else}
-    <slot name="fallback" />
-  {/if}
 </ClerkLoaded>
