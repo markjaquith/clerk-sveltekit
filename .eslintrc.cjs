e037d9e158e69669379a7656ee1afdfdc88f92d0
wobsoriano <sorianorobertc@gmail.com>
2024-08-13 08:56:28 -0700

    chore: Fix eslint error for unused 77987Props

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,4 +27,13 @@ module.exports = {
 			},
 		},
 	],
+	rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^(\\$\\$(Props|Events|Slots|Generic)|_)',
+			},
+    ]
+	}
 }
