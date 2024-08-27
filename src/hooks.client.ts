93e923201d8c8e09fda6e5404ddd0d41a98a62dc
wobsoriano <sorianorobertc@gmail.com>
2024-08-27 13:15:48 -0700

    chore: Hardcode sign in and sign up paths

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,18 +1,14 @@
 import type { HandleClientError } from '@sveltejs/kit'
 import { initializeClerkClient } from './lib/client/index.js'
 
-import {
-	PUBLIC_CLERK_PUBLISHABLE_KEY,
-	PUBLIC_CLERK_SIGN_IN_URL,
-	PUBLIC_CLERK_SIGN_UP_URL,
-} from '$env/static/public'
+import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public'
 
 initializeClerkClient({
 	publishableKey: PUBLIC_CLERK_PUBLISHABLE_KEY,
 	signInForceRedirectUrl: '/admin',
 	signUpForceRedirectUrl: '/admin',
-	signInUrl: PUBLIC_CLERK_SIGN_IN_URL,
-	signUpUrl: PUBLIC_CLERK_SIGN_UP_URL,
+	signInUrl: '/sign-in',
+	signUpUrl: '/sign-up',
 })
 
 export const handleError: HandleClientError = async ({ error, event }) => {
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,12 @@
 import { redirect, type Handle } from '@sveltejs/kit'
 import { sequence } from '@sveltejs/kit/hooks'
 import { withClerkHandler } from '$lib/server'
-import { PUBLIC_CLERK_SIGN_IN_URL, PUBLIC_CLERK_SIGN_UP_URL } from '$env/static/public'
 
 export const handle: Handle = sequence(
 	withClerkHandler({
 		debug: false,
-		signInUrl: PUBLIC_CLERK_SIGN_IN_URL,
-		signUpUrl: PUBLIC_CLERK_SIGN_UP_URL,
+		signInUrl: '/sign-in',
+		signUpUrl: '/sign-up',
 	}),
 	({ event, resolve }) => {
 		const { userId } = event.locals.auth
