2202a23f9c12424321385925ae1d66f37db2d2a6
Mark Jaquith <mark@jaquith.me>
2024-10-31 12:23:29 -0400

    Do all the env var checks at once

--- e2e/global.setup.ts
+++ e2e/global.setup.ts
@@ -5,17 +5,17 @@ import dotenv from 'dotenv'
 setup('global setup', async () => {
 	dotenv.config()
 
-	if (!process.env.PUBLIC_CLERK_PUBLISHABLE_KEY) {
-		throw new Error('Please provide the PUBLIC_CLERK_PUBLISHABLE_KEY environment variable.')
+	if (
+		!process.env.PUBLIC_CLERK_PUBLISHABLE_KEY ||
+		!process.env.E2E_CLERK_USER_USERNAME ||
+		!process.env.E2E_CLERK_USER_PASSWORD
+	) {
+		throw new Error(
+			'Please provide PUBLIC_CLERK_PUBLISHABLE_KEY, E2E_CLERK_USER_USERNAME, and E2E_CLERK_USER_PASSWORD environment variables.'
+		)
 	}
 
 	await clerkSetup({
 		publishableKey: process.env.PUBLIC_CLERK_PUBLISHABLE_KEY,
 	})
-
-	if (!process.env.E2E_CLERK_USER_USERNAME || !process.env.E2E_CLERK_USER_PASSWORD) {
-		throw new Error(
-			'Please provide E2E_CLERK_USER_USERNAME and E2E_CLERK_USER_PASSWORD environment variables.'
-		)
-	}
 })
