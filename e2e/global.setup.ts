08fb59e314b3552533d1b67cad21b2e1cd42e7ba
Mark Jaquith <mark@jaquith.me>
2024-10-31 12:20:42 -0400

    Use dotenv to set up process.env and provide the public Clerk key

--- e2e/global.setup.ts
+++ e2e/global.setup.ts
@@ -1,8 +1,17 @@
 import { clerkSetup } from '@clerk/testing/playwright'
 import { test as setup } from '@playwright/test'
+import dotenv from 'dotenv'
 
 setup('global setup', async () => {
-	await clerkSetup()
+	dotenv.config()
+
+	if (!process.env.PUBLIC_CLERK_PUBLISHABLE_KEY) {
+		throw new Error('Please provide the PUBLIC_CLERK_PUBLISHABLE_KEY environment variable.')
+	}
+
+	await clerkSetup({
+		publishableKey: process.env.PUBLIC_CLERK_PUBLISHABLE_KEY,
+	})
 
 	if (!process.env.E2E_CLERK_USER_USERNAME || !process.env.E2E_CLERK_USER_PASSWORD) {
 		throw new Error(
